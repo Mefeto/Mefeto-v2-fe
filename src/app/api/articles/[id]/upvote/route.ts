d0682d288a05e80c5639fcf696f6600508f71268
2paperstar <lee@paperst.ar>
2023-07-18 14:36:03 +0900

    feat: upvote

---
+++ src/app/api/articles/[id]/upvote/route.ts
@@ -0,0 +1,32 @@
+import { auth } from "@clerk/nextjs";
+import { sql } from "@vercel/postgres";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { userId } = auth();
+  if (!userId) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+  const { id } = params;
+
+  const articleCount = await sql`
+    SELECT COUNT(*) FROM articles WHERE id = ${id}
+  `;
+  if (articleCount.rows[0].count === 0) {
+    return new Response("Article not found", { status: 404 });
+  }
+
+  try {
+    const res = await sql`
+    INSERT INTO article_upvotes (article_id, user_id)
+    VALUES (${id}, ${userId})
+    RETURNING article_id
+  `;
+    return NextResponse.json(res.rows[0]);
+  } catch (e) {
+    return new Response("Already upvoted", { status: 409 });
+  }
+}
