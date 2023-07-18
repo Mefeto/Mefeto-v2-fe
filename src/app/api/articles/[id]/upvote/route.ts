16a03f42413dbb87ad1120dd2a4b96a077759c68
2paperstar <lee@paperst.ar>
2023-07-18 15:17:08 +0900

    feat: delete upvote

--- src/app/api/articles/[id]/upvote/route.ts
+++ src/app/api/articles/[id]/upvote/route.ts
@@ -13,7 +13,7 @@ export async function POST(
   const { id } = params;
 
   const articleCount = await sql`
-    SELECT COUNT(*) FROM articles WHERE id = ${id}
+    SELECT COUNT(*)::int FROM articles WHERE id = ${id}
   `;
   if (articleCount.rows[0].count === 0) {
     return new Response("Article not found", { status: 404 });
@@ -29,4 +29,29 @@ export async function POST(
   } catch (e) {
     return new Response("Already upvoted", { status: 409 });
   }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { userId } = auth();
+  if (!userId) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+  const { id } = params;
+
+  const articleCount = await sql`
+    SELECT COUNT(*)::int FROM articles WHERE id = ${id}
+  `;
+  if (articleCount.rows[0].count === 0) {
+    return new Response("Article not found", { status: 404 });
+  }
+
+  const res = await sql`
+    DELETE FROM article_upvotes
+    WHERE article_id = ${id} AND user_id = ${userId}
+    RETURNING article_id
+  `;
+  return NextResponse.json(res.rows[0]);
 }
